c49f29aeada69b0f8f3f774614b4b0d72a19c7c5
Melvin Cheung <mfcheung@mae.cuhk.edu.hk>
2014-11-04 14:53:07 +0800

    isolate kmeans function

--- test/extract_color_by_kmean.m
+++ test/extract_color_by_kmean.m
@@ -28,28 +28,8 @@ else
     ncols = size(ab,2);
     ab = reshape(ab,nrows*ncols,3);
 end
-% no_of_layers = 4;
-% repeat timage clustering 3 times to avoid local minima
 
-% cluster by predetermined seeds (under construction)
-if use_lab_transform
-    pad = [150, 152];
-    white_ink = [122, 127];
-    green_ink = [85, 148];
-    back_ground = [120, 128];
-else
-    pad = [236, 173, 146];
-    white_ink = [220, 249, 255];
-    green_ink = [0, 138, 80];
-    back_ground = [64, 78, 72];
-end
-predeter_seeds = [pad ; white_ink; green_ink; back_ground];
-
-% [cluster_idx, cluster_center] = kmeans(ab,no_of_layers,'distance','sqEuclidean', ...
-%     'start', predeter_seeds);
-
-[cluster_idx, cluster_center] = kmeans(ab,no_of_layers,'distance','sqEuclidean', ...
-                                      'Replicates',3, 'start', 'cluster', 'EmptyAction', 'singleton');
+[cluster_idx, cluster_center] = kmeans_clustering(ab, no_of_layers, use_lab_transform);
 
 % plot_color_dist(ab, 300, seeds, cluster_center cluster_idx);
 plot_color_dist(ab, 300, cluster_center, cluster_idx);
@@ -76,13 +56,38 @@ end
 % figure(); imshow(segmented_images{2}), title('objects in cluster 2');
 % output = segmented_images{2};
 % figure(); imshow(segmented_images{3}), title('objects in cluster 3');
-% 
+%
 % figure(); imshow(segmented_images{4}), title('objects in cluster 4');
-% 
+%
 % figure(); imshow(segmented_images{5}), title('objects in cluster 5');
-% 
+%
 % figure(); imshow(segmented_images{6}), title('objects in cluster 6');
 output = segmented_images{1};
+end
+
+function [cluster_idx, cluster_center] = kmeans_clustering(ab, no_of_layers, use_lab_transform)
+% no_of_layers = 4;
+% repeat timage clustering 3 times to avoid local minima
+
+% cluster by predetermined seeds (this part should be amended to non-hard code version)
+if use_lab_transform
+    pad = [150, 152];
+    white_ink = [122, 127];
+    green_ink = [85, 148];
+    back_ground = [120, 128];
+else
+    pad = [236, 173, 146];
+    white_ink = [220, 249, 255];
+    green_ink = [0, 138, 80];
+    back_ground = [64, 78, 72];
+end
+predeter_seeds = [pad ; white_ink; green_ink; back_ground];
+
+% [cluster_idx, cluster_center] = kmeans(ab,no_of_layers,'distance','sqEuclidean', ...
+%     'start', predeter_seeds);
+
+[cluster_idx, cluster_center] = kmeans(ab,no_of_layers,'distance','sqEuclidean', ...
+    'Replicates',3, 'start', 'cluster', 'EmptyAction', 'singleton');
 end
 
 function plot_color_dist(image, sample_size, cluster_center, cluster_idx)
@@ -102,12 +107,12 @@ figure(); hold on;
 title('CIELAB transformed color distribution');
 xlabel('red <---> green');
 ylabel('blue <---> yellow');
-cluster_color = rand([size(cluster_center,1), 3]);%generate a set of 
-    % random colors for initial and final position of cluster centers
+cluster_color = rand([size(cluster_center,1), 3]);%generate a set of
+% random colors for initial and final position of cluster centers
 for i = 1: max(sample(:, 3));
     cluster_data = sample(sample(:, 3) ==i, 1:2);
     scatter(cluster_data(:,1), cluster_data(:,2), [], cluster_color(i, :), '.');
-%     scatter(sample(:,1), sample(:,2), '.');
+    %     scatter(sample(:,1), sample(:,2), '.');
 end
 legend('Data point', 'Location', 'best');
 
@@ -117,6 +122,6 @@ marker_area = 64; %marker size
 %         center_color, 'd', 'filled');
 %plot final cluster centers
 scatter(cluster_center(:,1), cluster_center(:,2), marker_area, ...
-        cluster_color, 'o', 'filled');
+    cluster_color, 'o', 'filled');
 hold off;
 end
