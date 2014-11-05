81f1e80ca993c987c05ff45e40497727d16135e5
Melvin Cheung <mfcheung@mae.cuhk.edu.hk>
2014-11-05 09:28:44 +0800

    draft the hierarchical clust memory problem solution

--- test/hierarchical_clustering.m
+++ test/hierarchical_clustering.m
@@ -1,4 +1,17 @@
 function cluster_idx = hierarchical_clustering(image, cutoff)
 keyboard;
+% Current problem:
+% does not have enough ram to do clustering for the whole data set
+% n observations needs (n+1)*n/2 storage space
+% Procedures:
+% get the memory available
+% repeat
+%     random sample the data with size within available memory
+%     build partial cluster centroids
+% until all data are clustered
+% cluster the centroids
+% reference:
+% https://stackoverflow.com/questions/2946087/out-of-memory-error-while-using-clusterdata-in-matlab
+
 cluster_idx = clusterdata(image, cutoff);
 end
